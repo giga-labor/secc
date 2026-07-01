befaddc67a1a0865bbb7bf6cb0ba7895efb12d17
GiGa <giuseppegalati74-crypto@users.noreply.github.com>
2026-07-01 21:50:57 +0200

    chore: secc admin publish

--- assets/js/algorithms.js
+++ assets/js/algorithms.js
@@ -99,8 +99,8 @@ async function renderAlgorithms(area, algorithms, spotlightCards = []) {
   const tabsSheet = document.createElement('div');
   tabsSheet.className = 'tabs-sheet';
 
-  const CATEGORY_LABELS = { statistici: 'Statistici', neurali: 'Neurali', ibridi: 'Ibridi', generativi: 'G-Family', algoritmo: 'Algoritmo Family' };
-  const CATEGORY_ORDER = ['statistici', 'neurali', 'ibridi', 'generativi', 'algoritmo'];
+  const CATEGORY_LABELS = { statistici: 'Statistici', neurali: 'Neurali', ibridi: 'Ibridi', generativi: 'G-Family', algoritmo: 'Algoritmo Family', genetico: 'Genetico Family' };
+  const CATEGORY_ORDER = ['statistici', 'neurali', 'ibridi', 'generativi', 'algoritmo', 'genetico'];
 
   const buckets = {};
   algorithms.forEach((algorithm) => {
